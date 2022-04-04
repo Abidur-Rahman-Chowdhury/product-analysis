c9d291db322e3e550d3ad483a5f022327bb16cec
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-05 01:59:27 +0600

    done about component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Blogs from './components/Blogs/Blogs';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
@@ -20,7 +21,7 @@ function App() {
         <Route path='/reviews' element={<Review></Review>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route path='/about' element></Route>
+        <Route path='/about' element={<About></About>}></Route>
         <Route path='/*' element={<NotFound></NotFound>}></Route>
       </Routes>
       
---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='max-w-7xl mx-auto text-center'>
+            <h1 className='text-center text-5xl font-bold border-b-2 inline-block pb-5 border-orange-500'>About <span className='text-orange-500'>Us</span></h1>
+            <p className='mt-4 text-xl text-gray-700'> We are the best choice for you if you trust on our service. We provide best foods with reasonable price. We never compromise the quality of food we serve to you. We choose the good quality foods to give you the best of us. Hope you enjoy the every bite of our foods. We have all kinds of food recipe  Bangla, Chinese, Italian, Western Indian Everything. So dont hesitate to taste our food.  </p>
+        </div>
+    );
+};
+
+export default About;
